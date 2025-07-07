7ee927f13210835ed2dc185899b348cc0964f50d
revenoirr <federico.gym36@gmail.com>
2025-07-07 16:14:40 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
     <Provider store={store}>
       <AuthProvider>
         <ThemeProvider>
-          <Router basename="/food-orderReact">
+          <Router>
             <div className="app">
               <Header />
               <div className="main-content">
